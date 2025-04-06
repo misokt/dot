08b7160ec3e2b4e78f0002c45aa9fd7a59e41471
opp <49080343+opp@users.noreply.github.com>
2025-04-05 21:13:56 -0400

    Emacs: package list update

--- home/.emacs.custom.el
+++ home/.emacs.custom.el
@@ -8,7 +8,7 @@
    '("1361c7339c679611a7402045390332058b635f14c1c5d119e94279b5c8f998f9"))
  '(dired-listing-switches "-alhv")
  '(package-selected-packages
-   '(markdown-mode ido-completing-read+ go-mode move-text company)))
+   '(company go-mode ido-completing-read+ markdown-mode move-text smex)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
