06805b44c070cadeb0c59d2b2abfccd532214ccd
opp <49080343+000000d@users.noreply.github.com>
2025-04-28 21:41:49 -0400

    Proper indent

--- deploy.c
+++ deploy.c
@@ -14,91 +14,91 @@
 
 void create_symlink(const char *source, const char *target)
 {
-  if (symlink(source, target) != 0) {
-    fprintf(stderr, "error creating symlink. ERROR: %s -> %s\n", source, target);
-  } else {
-    printf("Created symlink from %s to %s\n", source, target);
-  }
+    if (symlink(source, target) != 0) {
+        fprintf(stderr, "error creating symlink. ERROR: %s -> %s\n", source, target);
+    } else {
+        printf("Created symlink from %s to %s\n", source, target);
+    }
 }
 
 void visit_directory(Map *map, const char *key, const char *path)
 {
-  struct dirent *entry;
-  struct stat st;
+    struct dirent *entry;
+    struct stat st;
 
-  char cwd[MAX_PATH_LENGTH];
-  getcwd(cwd, sizeof(cwd));
-  assert(cwd != NULL);
+    char cwd[MAX_PATH_LENGTH];
+    getcwd(cwd, sizeof(cwd));
+    assert(cwd != NULL);
 
-  DIR *dir = opendir(path);
-  if (dir == NULL) {
-    fprintf(stderr, "error opening directory. ERROR: %s\n", path);
-    return;
-  }
-
-  while ((entry = readdir(dir)) != NULL) {
-    if (STRCMP(entry->d_name, ".", "..")) {
-      continue;
+    DIR *dir = opendir(path);
+    if (dir == NULL) {
+        fprintf(stderr, "error opening directory. ERROR: %s\n", path);
+        return;
     }
 
-    char full_path[MAX_PATH_LENGTH];
-    snprintf(full_path, sizeof(full_path), "%s/%s", path, entry->d_name);
+    while ((entry = readdir(dir)) != NULL) {
+        if (STRCMP(entry->d_name, ".", "..")) {
+            continue;
+        }
 
-    if (stat(full_path, &st) == -1) {
-      fprintf(stderr, "error getting file status. ERROR: %s\n", full_path);
-      continue;
+        char full_path[MAX_PATH_LENGTH];
+        snprintf(full_path, sizeof(full_path), "%s/%s", path, entry->d_name);
+
+        if (stat(full_path, &st) == -1) {
+            fprintf(stderr, "error getting file status. ERROR: %s\n", full_path);
+            continue;
+        }
+
+        if (S_ISDIR(st.st_mode)) {
+            visit_directory(map, key, full_path);
+        } else if (S_ISREG(st.st_mode)) {
+            const char *target_dir = get_value(map, key);
+            if (target_dir != NULL) {
+                char target_path[MAX_PATH_LENGTH];
+                snprintf(target_path, sizeof(target_path), "%s%s", target_dir, (full_path+strlen(key)));
+
+                char source_path[MAX_PATH_LENGTH*2];
+                snprintf(source_path, sizeof(source_path), "%s/%s", cwd, full_path);
+
+                create_symlink(source_path, target_path);
+            }
+        }
     }
 
-    if (S_ISDIR(st.st_mode)) {
-      visit_directory(map, key, full_path);
-    } else if (S_ISREG(st.st_mode)) {
-      const char *target_dir = get_value(map, key);
-      if (target_dir != NULL) {
-        char target_path[MAX_PATH_LENGTH];
-        snprintf(target_path, sizeof(target_path), "%s%s", target_dir, (full_path+strlen(key)));
-
-        char source_path[MAX_PATH_LENGTH*2];
-        snprintf(source_path, sizeof(source_path), "%s/%s", cwd, full_path);
-
-        create_symlink(source_path, target_path);
-      }
-    }
-  }
-
-  closedir(dir);
+    closedir(dir);
 }
 
 Map* init_config()
 {
-  Map *map = create_map();
+    Map *map = create_map();
 
-  char *home_dir = getenv("HOME");
-  assert(home_dir != NULL);
-  add_to_map(map, "home", home_dir);
+    char *home_dir = getenv("HOME");
+    assert(home_dir != NULL);
+    add_to_map(map, "home", home_dir);
 
-  char config_dir[MAX_PATH_LENGTH];
-  snprintf(config_dir, sizeof(config_dir), "%s/%s", home_dir, ".config");
-  add_to_map(map, "config", config_dir);
+    char config_dir[MAX_PATH_LENGTH];
+    snprintf(config_dir, sizeof(config_dir), "%s/%s", home_dir, ".config");
+    add_to_map(map, "config", config_dir);
 
-  char emacs_d_dir[MAX_PATH_LENGTH];
-  snprintf(emacs_d_dir, sizeof(emacs_d_dir), "%s/%s", home_dir, ".emacs.d");
-  add_to_map(map, "emacs.d", emacs_d_dir);
+    char emacs_d_dir[MAX_PATH_LENGTH];
+    snprintf(emacs_d_dir, sizeof(emacs_d_dir), "%s/%s", home_dir, ".emacs.d");
+    add_to_map(map, "emacs.d", emacs_d_dir);
 
-  // char *root_dir = "/";
-  // add_to_map(map, "root", root_dir);
+    // char *root_dir = "/";
+    // add_to_map(map, "root", root_dir);
 
-  return map;
+    return map;
 }
 
 int main(void)
 {
-  Map *map = init_config();
+    Map *map = init_config();
 
-  char **keys = get_keys(map);
-  for (size_t i = 0; i < map->size; ++i) {
-    visit_directory(map, keys[i], keys[i]);
-  }
+    char **keys = get_keys(map);
+    for (size_t i = 0; i < map->size; ++i) {
+        visit_directory(map, keys[i], keys[i]);
+    }
 
-  free_map(map);
-  return 0;
+    free_map(map);
+    return 0;
 }
--- map.h
+++ map.h
@@ -2,14 +2,14 @@
 #define MAP_H_
 
 typedef struct {
-  char *key;
-  char *value;
+    char *key;
+    char *value;
 } KV;
 
 #include <stdlib.h>
 typedef struct {
-  KV *pair;
-  size_t size;
+    KV *pair;
+    size_t size;
 } Map;
 
 Map* create_map();
@@ -24,46 +24,46 @@ void free_map(Map *map);
 
 Map* create_map()
 {
-  Map *map = (Map*)malloc(sizeof(Map));
-  map->pair = NULL;
-  map->size = 0;
-  return map;
+    Map *map = (Map*)malloc(sizeof(Map));
+    map->pair = NULL;
+    map->size = 0;
+    return map;
 }
 
 #include <string.h>
 void add_to_map(Map *map, const char *key, const char *value)
 {
-  map->pair = (KV*)realloc(map->pair, (size_t)((map->size + 1) * sizeof(KV)));
-  map->pair[map->size].key = strdup(key);
-  map->pair[map->size].value = strdup(value);
-  map->size++;
+    map->pair = (KV*)realloc(map->pair, (size_t)((map->size + 1) * sizeof(KV)));
+    map->pair[map->size].key = strdup(key);
+    map->pair[map->size].value = strdup(value);
+    map->size++;
 }
 
 char** get_keys(Map *map)
 {
-  char **keys = malloc(sizeof(char*)*map->size);
-  for (size_t i = 0; i < map->size; ++i)
-    keys[i] = map->pair[i].key;
-  return keys;
+    char **keys = malloc(sizeof(char*)*map->size);
+    for (size_t i = 0; i < map->size; ++i)
+        keys[i] = map->pair[i].key;
+    return keys;
 }
 
 char* get_value(Map *map, const char *key)
 {
-  for (size_t i = 0; i < map->size; ++i) {
-    if (strcmp(map->pair[i].key, key) == 0)
-      return map->pair[i].value;
-  }
-  return NULL;
+    for (size_t i = 0; i < map->size; ++i) {
+        if (strcmp(map->pair[i].key, key) == 0)
+            return map->pair[i].value;
+    }
+    return NULL;
 }
 
 void free_map(Map *map)
 {
-  for (size_t i = 0; i < map->size; ++i) {
-    free(map->pair[i].key);
-    free(map->pair[i].value);
-  }
-  free(map->pair);
-  free(map);
+    for (size_t i = 0; i < map->size; ++i) {
+        free(map->pair[i].key);
+        free(map->pair[i].value);
+    }
+    free(map->pair);
+    free(map);
 }
 
 #endif // MAP_IMPLEMENTATION
